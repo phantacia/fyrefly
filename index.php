20190ffe0f08d728571d314f08054bdc9062a32d
phantacia <patriceeandrea@gmail.com>
2024-06-29 17:47:19 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+/** 
+ * Main template file
+ * 
+ * @package Fyrefly
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fyrefly</title>
+</head>
+<body>
+    Hello, World!
+</body>
+</html>
